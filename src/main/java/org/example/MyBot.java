da5beb7970a63b2352363f1a5c0adc364f0deb52
Marina Khrystyianyn <svitlamarina@gmail.com>
2024-02-17 13:42:27 +0100

    4 lesson task 1 add pictures

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -3,6 +3,8 @@ package org.example;
 import net.thauvin.erik.crypto.CryptoPrice;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -21,23 +23,37 @@ public class MyBot extends TelegramLongPollingBot {
        if(text.equals("/start")){
            sendMessage(chatId,"HALLO");
        } else if (text.equals("/all")){
-           sendPrice(chatId, "BTC");
-           sendPrice(chatId, "ETH");
-           sendPrice(chatId, "DOGE");
+           sendCript(chatId, "btc");
+           sendCript(chatId, "eth");
+           sendCript(chatId, "doge");
        } else {
            for (int i = 0; i < words.length; i++) {
-               if (words[i].equals("btc")){
-                   sendPrice(chatId, "BTC");
-               }else if(words[i].equals("eth")){
-                   sendPrice(chatId, "ETH");
-               }else if(words[i].equals("doge")){
-                   sendPrice(chatId, "DOGE");
-               }else sendMessage(chatId, "Unknown command!");
+               sendCript(chatId,words[i]);
            }
        }
      } catch (Exception e) {
             System.out.println("Error!");
         }
+    }
+
+    void sendCript(long chatId, String name) throws Exception{
+        if (name.equals("btc")){
+            sendPicture(chatId, "bitcoin.png");
+            sendPrice(chatId, "BTC");
+        }else if(name.equals("eth")){
+            sendPicture(chatId, "ethereum.png");
+            sendPrice(chatId, "ETH");
+        }else if(name.equals("doge")){
+            sendPicture(chatId, "dogecoin.png");
+            sendPrice(chatId, "DOGE");
+        }else sendMessage(chatId, "Unknown command!");
+    }
+    void sendPicture(long chatId, String name) throws Exception {
+        var photo = getClass().getClassLoader().getResourceAsStream(name);
+        var message = new SendPhoto();
+        message.setChatId(chatId);
+        message.setPhoto(new InputFile(photo, name));
+        execute(message);
     }
     void sendPrice(long chatId, String name) throws Exception {
         var price = CryptoPrice.spotPrice(name);
